143ac4e56f428ae659d4469850533571667f79e8
wcq1 <wcq1@meitu.com>
2017-06-18 19:46:06 +0800

    out clear

--- commons/src/com/superstudio/commons/MvcResources.java
+++ commons/src/com/superstudio/commons/MvcResources.java
@@ -9,12 +9,12 @@ public class MvcResources {
 	public static final String Common_NullOrEmpty = "Common_NullOrEmpty {0}";
 	public static final String DependencyResolver_DoesNotImplementICommonServiceLocator = "DependencyResolver_DoesNotImplementICommonServiceLocator";
 	public static final Object Common_PropertyCannotBeNullOrEmpty = "Common_PropertyCannotBeNullOrEmpty";
-	public static final String CshtmlView_ViewCouldNotBeCreated = "Common_PropertyCannotBeNullOrEmpty";
-	public static final String CshtmlView_WrongViewBase = "Common_PropertyCannotBeNullOrEmpty";
+	public static final String JhtmlView_ViewCouldNotBeCreated = "Common_PropertyCannotBeNullOrEmpty";
+	public static final String JhtmlView_WrongViewBase = "Common_PropertyCannotBeNullOrEmpty";
 	public static final String ViewStartPage_RequiresMvcRazorView = "Common_PropertyCannotBeNullOrEmpty";
 
 	public static final String Common_TemplateNotFound ="template ({0}) not found ，already search for locations:{1}";
-	public static String CshtmlTemplate_TemplateCouldNotBeCreated="CshtmlTemplate_TemplateCouldNotBeCreated.{0}";
+	public static String JhtmlTemplate_TemplateCouldNotBeCreated="JhtmlTemplate_TemplateCouldNotBeCreated.{0}";
 	public static String TemplateStartPage_RequiresMvcRazorTemplate="TemplateStartPage_RequiresMvcRazorTemplate";
-	public static String CshtmlTemplate_WrongTemplateBase="TemplateStartPage_RequiresMvcRazorTemplate";
+	public static String JhtmlTemplate_WrongTemplateBase="TemplateStartPage_RequiresMvcRazorTemplate";
 }
